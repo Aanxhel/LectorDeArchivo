c79de53aec9f0c50812ddc258cdf82a95fad248f
aanxhel <ang.core92@yahoo.com>
2021-02-05 01:33:00 -0600

    documentacion de pestañas

--- FormatArchivo/src/formatarchivo/Operadores.java
+++ FormatArchivo/src/formatarchivo/Operadores.java
@@ -6,23 +6,25 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Operadores {
-    public void analizarRuta(String ruta){
-        File archivoSinFormato = new File( ruta );
+    
+    //analizador de existencia de archivo
+    public void analizarRuta(String r){              //cara de ruta de archivo
+        File archivoSinFormato = new File( r );
         
         if(archivoSinFormato.exists()){             //verificar si existe archivo
             
             System.out.println("la ruta existe");
-        }else{
+        }else{                                      // en caso de no exitir mensa de salida
             System.out.println("no existe");
-        }
+        }//fin de IF/ELSE
         
-    }
+    }//fin void analizarRuta
     
-    
+    //clase que lee el archivo e imprime en consola
     public void lecturaArchivo(String ruta){
         
         try {
-            FileReader r = new FileReader(ruta);
+            FileReader r = new FileReader(ruta);                //carga del archivo texto
             BufferedReader buffer = new BufferedReader(r);
             
             //System.out.println(buffer.readLine());// me imprime linea por linea
@@ -40,5 +42,5 @@ public class Operadores {
         } catch (Exception ex) {
             System.out.println("sin archivo");
         }
-    }
+    }// fin void lecturaArchivo
 }
