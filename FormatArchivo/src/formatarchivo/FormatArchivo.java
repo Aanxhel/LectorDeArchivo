c412b1432ac80b7b9e504ddf99b6e2d618fc2f7a
aanxhel <ang.core92@yahoo.com>
2021-02-05 02:13:39 -0600

    se crea metodo de salida del programa

--- FormatArchivo/src/formatarchivo/FormatArchivo.java
+++ FormatArchivo/src/formatarchivo/FormatArchivo.java
@@ -1,20 +1,22 @@
 
 package formatarchivo;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FormatArchivo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        String locaArchivo = "./Archivo/asino.txt";
-        
+        String locaArchivo = "./Archivo/archivo.txt";
+        String locaSalisa = "./Archivo/archivoSalida.txt";
         
         Operadores archivo = new Operadores();
-        
+
         archivo.analizarRuta(locaArchivo);
         archivo.lecturaArchivo(locaArchivo);
+        archivo.escribirArchivo(locaSalisa);
         
-    }
+    }// static void main
     
-}
+}//fin class FormatArchivo
--- FormatArchivo/src/formatarchivo/Operadores.java
+++ FormatArchivo/src/formatarchivo/Operadores.java
@@ -1,4 +1,3 @@
-
 package formatarchivo;
 
 import java.io.*;
@@ -6,41 +5,48 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Operadores {
-    
+
     //analizador de existencia de archivo
-    public void analizarRuta(String r){              //cara de ruta de archivo
-        File archivoSinFormato = new File( r );
-        
-        if(archivoSinFormato.exists()){             //verificar si existe archivo
-            
+    public void analizarRuta(String r) {              //cara de ruta de archivo
+        File archivoSinFormato = new File(r);           // File compruba que exista el archivo
+        if (archivoSinFormato.exists()) {             //verificar si existe archivo
             System.out.println("la ruta existe");
-        }else{                                      // en caso de no exitir mensa de salida
-            System.out.println("no existe");
+        } else {                                      // en caso de no exitir mensa de salida
+            System.out.println("no existe y hay que crearlo");
         }//fin de IF/ELSE
-        
     }//fin void analizarRuta
-    
+
     //clase que lee el archivo e imprime en consola
-    public void lecturaArchivo(String ruta){
-        
+    public void lecturaArchivo(String ruta) {
+
         try {
             FileReader r = new FileReader(ruta);                //carga del archivo texto
             BufferedReader buffer = new BufferedReader(r);
-            
+
             //System.out.println(buffer.readLine());// me imprime linea por linea
-            
             //lectura del archivo de texto e imprecion en consola
             String temp = "";
-            
-            while(temp != null){
+            while (temp != null) {                // buscador de archivo todas las lineas
                 temp = buffer.readLine();
-                if(temp == null)break;
+                if (temp == null) {
+                    break;
+                }
                 System.out.println(temp);
-            }
-            
-            
+            }//fin while
+
         } catch (Exception ex) {
             System.out.println("sin archivo");
         }
     }// fin void lecturaArchivo
-}
+    
+    public void escribirArchivo(String r)throws IOException{
+        
+        File archivo = new File(r);
+        
+        BufferedWriter bw;                              //buffer para escribir
+        
+        bw = new BufferedWriter(new FileWriter(archivo));
+        bw.write("El fichero de texto ya estaba creado.");
+        
+    }
+}//fin class Operadores
