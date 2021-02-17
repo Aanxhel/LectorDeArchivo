d05e4c1f74221159474b82c0738358558aaa7cc4
aanxhel <ang.core92@yahoo.com>
2021-02-17 14:17:42 -0600

    se crea archivo en c#

---
+++ FormatArchivo/FormatArchivo/ItemRead.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FormatArchivo
+{
+    internal class ItemRead
+    {
+        internal void testRead(object archivoIn)
+        {
+            Console.WriteLine(archivoIn);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FormatCSharp/ConsoleApp1/ConsoleApp1/ReadItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class ReadItem
+    {
+        internal void check(string item)
+        {
+            Console.WriteLine(item);
+            Console.ReadKey();
+        }
+    }
+}
