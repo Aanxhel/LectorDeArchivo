4e030575336d8af82db4a8567b5a7593de1437bd
aanxhel <ang.core92@yahoo.com>
2021-02-17 10:40:31 -0600

    otro nombre archivo local

--- exprecionRegular/src/exprecionregular/ExprecionRegular.java
+++ exprecionRegular/src/exprecionregular/ExprecionRegular.java
@@ -1,12 +1,8 @@
 package exprecionregular;
 
 public class ExprecionRegular {
-
     public static void main(String[] args) {
         caracter car = new caracter();
-
         car.busqueda();
-
     }
-
 }
